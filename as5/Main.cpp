11ee18a92da128038dd6019af0047bf3c16c1187
FerdiaMT <oscadot@gmail.com>
2025-12-12 22:06:22 +0000

    finished assingment 5

--- as5/Main.cpp
+++ as5/Main.cpp
@@ -5,29 +5,46 @@
 
 int main()
 {
-    std::cout << "=== Testing Module and Programme System ===" << std::endl;
+    std::cout << "=== SYSTEM TEST ====" << std::endl;
 
     // our two modules made for testing
     Module cs264("CS264", 181, 5);
     Module cs357("CS357", 155, 5);
 
+    // a second cs264, but with dif amount of students
     Module cs264CompareTest("CS264", 100, 5);
+
+    // test that the == just needs same name and credit
     if (cs264 == cs264CompareTest)
     {
-        std::cout << "CS264 and testModule1 are identical (same name and credits)" << std::endl;
+        std::cout << "TEST SUCESS: CS264 and testModule1 are identical (same name and credits)" << std::endl;
+    }
+    else
+    {
+        std::cout << "TEST FAIL: CS264 and testModule1 are not identical (not same name and credits)" << std::endl;
     }
 
+    // proof that == NEEDS same credits
     Module cs264CompareTestFail("CS264", 200, 2);
     if (!(cs264 == cs264CompareTestFail))
     {
-        std::cout << "CS264 and testModule2 are Different" << std::endl;
+        std::cout << "TEST SUCESS: CS264 and testModule2 are Different" << std::endl;
+    }
+    else
+    {
+        std::cout << "TEST FAIL: CS264 and testModule2 are same" << std::endl;
     }
 
     if (!(cs264 == cs357))
     {
-        std::cout << "CS264 and cs357 are Different " << std::endl;
+        std::cout << "TEST SUCESS: CS264 and cs357 are Different " << std::endl;
     }
-    std::cout << std::endl;
+    else
+    {
+        std::cout << "TEST FAIL: CS264 and cs357 are same " << std::endl;
+    }
+
+    std::cout << " ========================== " << std::endl;
 
     // now we can create our program
     Programme csse("CSSE (3rd Year)", 3, 6);
@@ -64,7 +81,7 @@ int main()
     csse.addModule(&extra5);
 
     std::cout << std::endl;
-    std::cout << "--- csse final ---" << std::endl;
+    std::cout << "---= final programme print =---" << std::endl;
     std::cout << csse.toStr();
 
     return 0;
--- as5/Programme.cpp
+++ as5/Programme.cpp
@@ -5,6 +5,14 @@
 Programme::Programme(std::string name, unsigned int year, unsigned int capacity)
     : _name(name), _year(year), capacity(capacity), currentSize(0)
 {
+
+    // check were not being asked to fill an empty program
+    if (capacity == 0)
+    {
+        modules = nullptr;
+        return;
+    }
+
     // create our array of modules, with the limit being set by capacity input
     modules = new Module *[capacity];
 
@@ -16,9 +24,20 @@ Programme::Programme(std::string name, unsigned int year, unsigned int capacity)
 }
 
 // copy constructor
+// i know this isnt required but just incase it ends up being worth something
+// as i know we had to make a copy constructor in all the other ones
+// so here it is
 Programme::Programme(const Programme &other)
     : _name(other._name), _year(other._year), capacity(other.capacity), currentSize(other.currentSize)
 {
+
+    // check were not being asked to fill an empty program
+    if (capacity == 0 || other.modules == nullptr)
+    {
+        modules = nullptr;
+        return;
+    }
+
     // create our array of modules, with the limit being set by capacity input
     modules = new Module *[capacity];
 
@@ -33,6 +52,7 @@ Programme::Programme(const Programme &other)
 Programme::~Programme()
 {
     delete[] modules;
+    modules = nullptr;
 }
 
 /////// Getters
@@ -72,6 +92,20 @@ void Programme::setYear(unsigned int year)
 // add module, this attatches the pointer of the module inside the array
 void Programme::addModule(Module *module)
 {
+    // being really defensive here because i heard other people getting
+    //  docked marks in assingment 3 due to something similair to this
+    if (module == nullptr)
+    {
+        std::cout << "module being added is null, cannot add" << std::endl;
+        return;
+    }
+
+    if (modules == nullptr)
+    {
+        std::cout << "module cannot be added as space to add is null" << std::endl;
+        return;
+    }
+
     // if we have remaining room in our array
     if (currentSize < capacity)
     {
@@ -84,28 +118,29 @@ void Programme::addModule(Module *module)
     else
     {
         // print incase its full
-        std::cout << "Cannot add module as the array is full. Capacity size: (" << capacity << ")" << std::endl;
+        std::cout << "cant add module as the array is full.  size: (" << capacity << ")" << std::endl;
     }
 }
 
 // to string
 std::string Programme::toStr() const
 {
-    std::string result = "Programme: " + _name + " (Year " + std::to_string(_year) + ")\n";
-    result += "Capacity: " + std::to_string(currentSize) + "/" + std::to_string(capacity) + " modules\n";
-    result += "Modules:\n";
+    std::string res = "Programme name: " + _name + " (Year " + std::to_string(_year) + ")\n";
+    res += "Module space remaning: " + std::to_string(currentSize) + "/" + std::to_string(capacity) + "\n";
+    res += "==================== \n";
+    res += "LIST OF MODULES:\n";
 
     for (unsigned int i = 0; i < currentSize; i++)
     {
         if (modules[i] != nullptr)
         {
-            result += "  - " + modules[i]->getName() +
-                      " (" + std::to_string(modules[i]->getCredits()) + " credits, " +
-                      std::to_string(modules[i]->getStudents()) + " students)\n";
+            res += modules[i]->getName();
+            res += " ( CREDITS:  " + std::to_string(modules[i]->getCredits()) + " , ";
+            res += " ( STUDENTS: " + std::to_string(modules[i]->getStudents()) + " )\n";
         }
     }
 
-    return result;
+    return res;
 }
 
 //  operator overloading
